e7a374d9323a34b609920f6dc6080f4eb21ceeab
Pilatron <talipsa@gmail.com>
2022-07-12 17:20:46 +0300

    b

--- loops/do_while.php
+++ loops/do_while.php
@@ -1,1 +1,13 @@
 <?php
+$i = 1;
+while ($i <= 7) {
+    echo $i++ . "<br>";
+}
+
+
+$x = 1;
+do
+{
+    echo "The number is: $x <br>";
+    $x++;
+} while ($x <= 5);
--- newifelse.php
+++ newifelse.php
@@ -1,1 +1,10 @@
 <?php
+    $a = 12;
+    $b = 13;
+    if ( $a > $b){
+        echo "a bigger then b";
+    } elseif ( $a == $b){
+        echo "a equal b";
+    } else echo "b bigger then a";
+
+
