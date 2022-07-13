6a80b6098ba2e1a714a8d477c9729b2ba1be5e77
Pilatron <talipsa@gmail.com>
2022-07-13 09:03:17 +0300

    b

--- loops/foreach.php
+++ loops/foreach.php
@@ -1,4 +1,18 @@
 <?php
-foreach ([1,2,3] as $key => $value) {
+foreach ([1,2,3] as $value) {
     echo $value . PHP_EOL;
+
+    echo "<br>";
 }
+
+$arr = array(1, 2, 3, 4);
+foreach ($arr as $key => &$value) {
+    $value = $value * 2;
+    $key++;
+    echo $key . " - "; echo PHP_EOL;
+    echo $value . "<br>";
+
+
+}
+// массив $arr сейчас таков: array(2, 4, 6, 8)
+unset($value); // разорвать ссылку на последний элемент
--- loops/switch.php
+++ loops/switch.php
@@ -1,1 +1,15 @@
 <?php
+    $i = 25;
+switch ($i) {
+    case 0:
+        echo "i равно 0";
+        break;
+    case 1:
+        echo "i равно 1";
+        break;
+    case 2:
+        echo "i равно 2";
+        break;
+    default:
+        echo "i точно больше 2";
+}
